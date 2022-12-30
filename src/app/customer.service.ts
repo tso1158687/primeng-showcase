5cd844f8c9ec285778f65a1ed0355a54e14a55cd
jason cheng <jason.cheng@lileesystems.com>
2022-12-30 14:50:25 +0800

    add vehicle data

--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -1,24 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay } from 'rxjs';
 import { Customer } from './table/table.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerService {
-
   constructor(private http: HttpClient) {}
 
+  getCustomersLarge() {
+    return this.http
+      .get<any>('assets/showcase/data/customers-large.json')
+      .toPromise()
+      .then((res) => <Customer[]>res.data)
+      .then((data) => {
+        return data;
+      });
+  }
 
-
-    getCustomersLarge() {
-        return this.http
-            .get<any>('assets/showcase/data/customers-large.json')
-            .toPromise()
-            .then((res) => <Customer[]>res.data)
-            .then((data) => {
-                return data;
-            });
-    }
-
+  getVehicleList() {
+    return this.http.get<any>('assets/showcase/data/vehicle.json');
+    // .pipe(delay(3000))
+  }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
 import { CustomerService } from '../customer.service';
 
 export interface Country {
@@ -15,7 +16,7 @@ export interface Customer {
   name?: string;
   country?: Country;
   company?: string;
-  date?: any ;
+  date?: any;
   status?: string;
   activity?: number;
   representative?: Representative;
@@ -26,16 +27,16 @@ export interface Customer {
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
-  customers: Customer[]= [];
+  customers: Customer[] = [];
 
-  selectedCustomers: Customer[]=[];
+  selectedCustomers: Customer[] = [];
 
-  representatives: Representative[]=[];
+  representatives: Representative[] = [];
 
-  statuses: any[]=[];
+  statuses: any[] = [];
 
   loading: boolean = true;
 
@@ -44,33 +45,41 @@ export class TableComponent {
   constructor(private customerService: CustomerService) {}
 
   ngOnInit() {
-      this.customerService.getCustomersLarge().then((customers) => {
-          this.customers = customers;
-          this.loading = false;
+    this.customerService
+      .getVehicleList()
+      .pipe(
+        map((e) => e.msg.list),
+        )
+      .subscribe((e) => console.log(e));
+    this.customerService.getCustomersLarge().then((customers) => {
+      this.customers = customers;
+      this.loading = false;
 
-          this.customers.forEach((customer) => (customer.date = new Date(customer.date)));
-      });
+      this.customers.forEach(
+        (customer) => (customer.date = new Date(customer.date))
+      );
+    });
 
-      this.representatives = [
-          { name: 'Amy Elsner', image: 'amyelsner.png' },
-          { name: 'Anna Fali', image: 'annafali.png' },
-          { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-          { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-          { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-          { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-          { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-          { name: 'Onyama Limba', image: 'onyamalimba.png' },
-          { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-          { name: 'Xuxue Feng', image: 'xuxuefeng.png' }
-      ];
+    this.representatives = [
+      { name: 'Amy Elsner', image: 'amyelsner.png' },
+      { name: 'Anna Fali', image: 'annafali.png' },
+      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+      { name: 'Onyama Limba', image: 'onyamalimba.png' },
+      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+      { name: 'Xuxue Feng', image: 'xuxuefeng.png' },
+    ];
 
-      this.statuses = [
-          { label: 'Unqualified', value: 'unqualified' },
-          { label: 'Qualified', value: 'qualified' },
-          { label: 'New', value: 'new' },
-          { label: 'Negotiation', value: 'negotiation' },
-          { label: 'Renewal', value: 'renewal' },
-          { label: 'Proposal', value: 'proposal' }
-      ];
+    this.statuses = [
+      { label: 'Unqualified', value: 'unqualified' },
+      { label: 'Qualified', value: 'qualified' },
+      { label: 'New', value: 'new' },
+      { label: 'Negotiation', value: 'negotiation' },
+      { label: 'Renewal', value: 'renewal' },
+      { label: 'Proposal', value: 'proposal' },
+    ];
   }
 }
